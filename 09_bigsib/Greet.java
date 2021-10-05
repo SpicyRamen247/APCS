515c007026e9b78209504ba181561b510fa6f754
SpicyRamen247 <jgao22@stuy.edu>
2021-10-05 02:56:48 -0400

    Created 2 programs
    Created method called BigSib.java which was then invoked in the Greet.java
    
    Used String name as an command-line argument . Note the String is the character type.

---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,21 @@
+/*
+Ariel Fuchs and the Duckler
+Ruby Friedman and Tiffany
+Joshua Gao and Batman
+
+2021-10-4
+
+DISCOVERIES
+ public static void greet(String name) refers to a method with an arugment as a string.
+ The String refers to the character type of the command-line argument.
+ "" indicates the String character.
+UNRESOLVED QUESTIONS
+*/
+
+public class Greet {
+    public static void main(String[] args) {
+        BigSib.greet("Film");
+        BigSib.greet("Flam");
+        BigSib.greet("Kazaam");
+    }
+}
