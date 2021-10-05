8976dac1e660df7e8f024dba0fc5056fca68161f
SpicyRamen247 <jgao22@stuy.edu>
2021-10-05 03:18:09 -0400

    Create Greet.Java

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,20 @@
+/*
+Joshua Gao and Batman
+
+2021-10-1
+
+DISCOVERIES
+n/a
+UNRESOLVED QUESTIONS
+n/a
+*/
+public class Greet {
+    public static void main(String[] args){
+        greet();
+    }
+    public static void greet(){
+        System.out.print("Why, hello there, Moo.");
+        System.out.print("How do you do?");
+    }
+}
+
